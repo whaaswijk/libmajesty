9f65479e33fedc0c37161282dc7d47697a2c195b
Winston Haaswijk <winston.haaswijk@epfl.ch>
2016-09-02 23:26:30 +0200

    Fixing bug in XMG copy constructor

--- libmajesty/src/xmg.cpp
+++ libmajesty/src/xmg.cpp
@@ -1157,12 +1157,17 @@ namespace majesty {
 	xmg::xmg(const xmg& sxmg) {
 		nodemap nodemap;
 		const auto& nodes = sxmg.nodes();
+		const auto& innames = sxmg.innames();
 		const auto nnodes = sxmg.nnodes();
 
 		for (auto i = 0u; i < nnodes; i++) {
 			const auto& node = nodes[i];
 			if (is_pi(node)) {
-				nodemap[i] = make_pair(create_input(), false);
+                if (i == 0u) {
+                    nodemap[i] = make_pair(create_input(), false);
+                } else {
+                    nodemap[i] = make_pair(create_input(innames[i - 1]), false);
+                }
 			} else if (is_xor(node)) {
 				auto in1 = nodemap[node.in1];
 				in1.second = (in1.second != is_c1(node));
