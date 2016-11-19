a9138accbd03b20633a36f4f1efded7f30c55dd5
Winston Haaswijk <winston.haaswijk@epfl.ch>
2016-11-19 17:36:10 +0100

    Updating classical rewriting

--- libmajesty/include/logic_rewriting.h
+++ libmajesty/include/logic_rewriting.h
@@ -261,9 +261,6 @@ namespace majesty {
 	cirkit::tt jake_canon(const cirkit::tt&, unsigned* uCanonPhase, char* pCanonPerm, unsigned num_vars);
 
 	template<typename S>
-	logic_ntk size_rewrite_strategy(const logic_ntk&, unsigned cut_size, unsigned conflict_limit, timeout_behavior = optimize_heuristically);
-
-	template<typename S>
 	logic_ntk lut_area_strategy(const logic_ntk& ntk, unsigned lut_size, unsigned conflict_limit = 0, timeout_behavior tb = optimize_heuristically) {
 		auto cut_params = default_cut_params();
 		cut_params->klut_size = lut_size;
@@ -302,4 +299,321 @@ namespace majesty {
 
 		return cntk;
 	}
+
+	int recursive_deselect(const nodeid nid, const std::vector<ln_node>& nodes, std::unordered_map<nodeid,unsigned>& nref) {
+		const auto& node = nodes[nid];
+		if (node.pi) {
+			return 0;
+		}
+		auto area = 1;
+		for (auto nodeid : node.fanin) {
+			nref[nodeid] -= 1;
+			if (nref[nodeid] == 0) {
+				area += recursive_deselect(nodeid, nodes, nref);
+			}
+		}
+		return area;
+	}
+
+	int recursive_select(const nodeid nid, const std::vector<ln_node>& nodes, std::unordered_map<nodeid,unsigned>& nref) {
+		const auto& node = nodes[nid];
+		if (node.pi) {
+			return 0;
+		}
+		auto area = 1;
+		const auto& fanin = node.fanin;
+		for (auto nodeid : fanin) {
+			nref[nodeid] += 1;
+			if (nref[nodeid] == 1) {
+				area += recursive_select(nodeid, nodes, nref);
+			}
+		}
+		return area;
+	}
+
+	int virtual_recursive_deselect(const logic_ntk& ntk, const logic_ntk& opt_ntk, 
+			const std::vector<nodeid>& fanin, const std::unordered_map<nodeid, nodeid>& nodemap, 
+			std::unordered_map<nodeid, unsigned>& nref) {
+		auto area = 0;
+		const auto& opt_ntk_nodes = opt_ntk.nodes();
+		const auto& nr_opt_ntk_nodes = opt_ntk.nnodes();
+
+		for (auto i = 0u; i < nr_opt_ntk_nodes; i++) {
+			const auto& node = opt_ntk_nodes[i];
+			if (node.pi) {
+				auto faninid = nodemap.at(fanin[i]); 
+				nref[faninid] -= 1;
+				if (nref.at(faninid) == 0) {
+					area += recursive_deselect(faninid, ntk.nodes(), nref);
+				}
+			} else {
+				++area;
+			}
+		}
+
+		return area;
+	}
+
+	int virtual_recursive_select(const logic_ntk& ntk, const logic_ntk& opt_ntk, 
+			const std::vector<nodeid>& fanin, const std::unordered_map<nodeid,nodeid>& nodemap, 
+			std::unordered_map<nodeid,unsigned>& nref) {
+		auto area = 0;
+		const auto& opt_ntk_nodes = opt_ntk.nodes();
+		const auto& nr_opt_ntk_nodes = opt_ntk.nnodes();
+
+		for (auto i = 0u; i < nr_opt_ntk_nodes; i++) {
+			const auto& node = opt_ntk_nodes[i];
+			if (node.pi) {
+				assert(nodemap.find(fanin[i]) != nodemap.end());
+				auto faninid = nodemap.at(fanin[i]); 
+				nref[faninid] += 1;
+				if (nref.at(faninid) == 1) {
+					area += recursive_select(faninid, ntk.nodes(), nref);
+				}
+			} else {
+				++area;
+			}
+		}
+
+		return area;
+	}
+
+	inline nodeid select_opt_ntk(logic_ntk& ntk, const logic_ntk& opt_ntk, const input_map_t& imap, 
+			bool invert_output, std::unordered_map<nodeid,unsigned>& nref) {
+		const auto& opt_ntk_nodes = opt_ntk.nodes();
+		const auto& nr_opt_ntk_nodes = opt_ntk.nnodes();
+
+		std::vector<std::pair<nodeid,bool>> nids(nr_opt_ntk_nodes);
+
+		for (auto i = 0u; i < nr_opt_ntk_nodes; i++) {
+			const auto& node = opt_ntk_nodes[i];
+			if (node.pi) {
+				nids[i] = imap.at(i);
+				const auto faninid = nids[i].first;
+				nref[faninid] += 1;
+				if (nref[faninid] == 1) {
+					recursive_select(faninid, ntk.nodes(), nref);
+				}
+			} else {
+				const auto gate_size = node.fanin.size();
+				const auto gate_tt_size = (1u << gate_size);
+				std::vector<nodeid> virtfanin;
+				for (auto j = 0u; j < gate_size; j++) {
+					virtfanin.push_back(nids[node.fanin[j]].first);
+				}
+				tt localfunc(gate_tt_size, 0);
+				for (auto j = 0u; j < gate_tt_size; j++) {
+					auto func_idx = 0u;
+					for (auto k = 0u; k < gate_size; k++) {
+						auto tbit = tt_nth_var(k).test(j);
+						if (nids[node.fanin[k]].second) {
+							tbit = !tbit;
+						}
+						func_idx += (tbit << k);
+					}
+					localfunc[j] = node.function[func_idx];
+				}
+				if ((i == nr_opt_ntk_nodes - 1) && invert_output) {
+					localfunc = ~localfunc;
+				}
+				auto new_nodeid = ntk.create_node(virtfanin, localfunc);
+				nids[i] = std::make_pair(new_nodeid, false);
+				nref[new_nodeid] = 1;
+			}
+		}
+
+		return nids[nr_opt_ntk_nodes - 1].first;
+	}
+
+	template<typename S>
+	logic_ntk logic_ntk_from_cuts(const logic_ntk& cut_ntk, const cutmap& cut_map, unsigned conflict_limit) {
+		logic_ntk tmp_ntk;
+		
+		std::unordered_map<nodeid,nodeid> nodemap;
+		std::unordered_map<nodeid,unsigned> nref;
+		function_store fstore;
+
+		const auto& nodes = cut_ntk.nodes();
+		const auto total_nodes = cut_ntk.nnodes();
+
+		funcmap fm = compute_all_functions(cut_ntk, cut_map);
+
+		auto progress = 0u;
+		for (auto i = 0u; i < total_nodes; i++) {
+			const auto& node = nodes[i];
+			if (node.pi) {
+				nodemap[i] = tmp_ntk.create_input();
+				++progress;
+				continue;
+			}
+			const auto& node_cuts = cut_map.at(i);
+			auto smallest_add = std::numeric_limits<unsigned>::max();
+			cut* best_cut = nullptr;
+			auto found_const_cut = false;
+
+			for (const auto& cut : node_cuts) {
+				const auto& cutfunc = *fm.at(cut.get());
+				if (cut->size() == 1 && cut->nodes()[0] == i) { // Trivial cut
+					continue;
+				} else if (cut->size() == 0) { // Const 1 or 0
+					found_const_cut = true;
+					if (cutfunc == tt_const0()) {
+						nodemap[i] = tmp_ntk.get_const0_node();
+					} else {
+						nodemap[i] = tmp_ntk.get_const1_node();
+					}
+					break;
+				} else {
+					std::vector<unsigned> perm; tt phase, npn;
+					const auto& cutnodes = cut->nodes();
+					if (cutnodes.size() < 6) {
+						npn = exact_npn_canonization(cutfunc, phase, perm);
+					} else {
+						npn = npn_canonization_lucky(cutfunc, phase, perm);
+					}
+					npn.resize(cutfunc.size());
+
+					synth_spec spec;
+					//spec.verbose = true;
+					spec.nr_vars = cut->size();
+					if (spec.nr_vars >= 3) {
+						// We have to manually set the gate size and gate_tt_size here,
+						// as exact synthesis may not be called when we use stored results.
+						// ES normally deduces these values, but if it's not called we
+						// still need them to parse the results back into the logic_ntk. 
+						spec.gate_size = 3;
+						spec.gate_tt_size = 7;
+					}
+					//auto opt_ntk_str = fstore.get_size_optimum_ntk_ns(cutfunc, &spec, conflict_limit);
+					auto opt_ntk = size_optimum_ntk_ns<S>(npn, &spec, conflict_limit);// //string_to_logic_ntk(opt_ntk_str.get());
+					if (opt_ntk) {
+						auto virt_nodes_added = virtual_recursive_select(tmp_ntk, opt_ntk.get(), cut->nodes(), nodemap, nref);
+						//auto virt_nodes_added = opt_ntk.nnodes();
+						if (virt_nodes_added < smallest_add) {
+							best_cut = cut.get();
+							smallest_add = virt_nodes_added;
+						}
+						auto virt_nodes_saved = virtual_recursive_deselect(tmp_ntk, opt_ntk.get(), cut->nodes(), nodemap, nref);
+						assert(virt_nodes_added == virt_nodes_saved);
+					}
+				}
+			}
+			if (found_const_cut) {
+				continue;
+			}
+			assert(best_cut != nullptr);
+			const auto& cutfunc = *fm.at(best_cut);
+			std::vector<unsigned> perm; tt phase, npn;
+			const auto& cutnodes = best_cut->nodes();
+			if (cutnodes.size() < 6) {
+				npn = exact_npn_canonization(cutfunc, phase, perm);
+			} else {
+				npn = npn_canonization_lucky(cutfunc, phase, perm);
+			}
+			npn.resize(cutfunc.size());
+
+			synth_spec spec;
+			//spec.verbose = true;
+			spec.nr_vars = best_cut->size();
+			if (spec.nr_vars >= 3) {
+				// We have to manually set the gate size and gate_tt_size here,
+				// as exact synthesis may not be called when we use stored results.
+				// ES normally deduces these values, but if it's not called we
+				// still need them to parse the results back into the logic_ntk. 
+				spec.gate_size = 3;
+				spec.gate_tt_size = 7;
+			}
+			//auto opt_ntk_str = fstore.get_size_optimum_ntk_ns(cutfunc, &spec, conflict_limit);
+			auto opt_ntk = size_optimum_ntk_ns<S>(npn, &spec);// string_to_logic_ntk(opt_ntk_str.get());
+
+			input_map_t imap;
+			auto invperm = inv(perm);
+			for (auto i = 0u; i < cutnodes.size(); i++) {
+				const auto inode = nodemap[cutnodes[i]];
+				imap[invperm[i]] = std::make_pair(inode, phase.test(i));
+			}
+
+			nodemap[i] = select_opt_ntk(tmp_ntk, opt_ntk.get(), imap, phase.test(cutnodes.size()), nref);
+			std::cout << "Progress: (" << ++progress << "/" << total_nodes << ")\r";
+		}
+		std::cout << std::endl;
+
+		nref.clear();
+		{
+			const auto& outputs = cut_ntk.outputs();
+			for (auto i = 0u; i < outputs.size(); i++) {
+				const auto nid = nodemap[outputs[i]];
+				tmp_ntk.create_output(nid);
+				nref[nid] = 1;
+				recursive_select(nid, tmp_ntk.nodes(), nref);
+			}
+		}
+
+		logic_ntk ntk;
+		auto tmp_nodes = tmp_ntk.nodes();
+		nodemap.clear();
+		for (auto i = 0u; i < tmp_ntk.nnodes(); i++) {
+			const auto& node = tmp_nodes[i];
+			if (node.pi) {
+				nodemap[i] = ntk.create_input();
+				++progress;
+			} else {
+				auto refs = nref[i];
+				if (refs > 0) {
+					std::vector<nodeid> nfanin;
+					for (auto id : node.fanin) {
+						nfanin.push_back(nodemap[id]);
+					}
+					nodemap[i] = ntk.create_node(nfanin, node.function);
+				}
+			}
+		}
+		
+		const auto& tmp_outputs = tmp_ntk.outputs();
+		for (auto i = 0u; i < tmp_outputs.size(); i++) {
+			const auto nid = nodemap[tmp_outputs[i]];
+			ntk.create_output(nid);
+		}
+
+		const auto& innames = cut_ntk.innames();
+		for (const auto& name : innames) {
+			ntk.add_inname(name);
+		}
+
+		const auto& outnames = cut_ntk.outnames();
+		for (const auto& name : outnames) {
+			ntk.add_outname(name);
+		}
+
+		return ntk;
+	}
+
+	template<typename S>
+	logic_ntk size_rewrite_strategy(const logic_ntk& ntk, unsigned cut_size, unsigned conflict_limit) {
+		auto cut_params = default_cut_params();
+		cut_params->klut_size = cut_size;
+	
+		logic_ntk cntk(ntk);
+		auto ctu = false;
+		do {
+			ctu = false;
+			funcmap fm;
+			auto oldsize = cntk.nnodes();
+			const auto cut_map = enumerate_cuts_eval_funcs(cntk, cut_params.get(), fm);
+			auto decomp_ntk = logic_ntk_from_cuts<S>(cntk, cut_map, conflict_limit);
+			auto newsize = decomp_ntk.nnodes();
+			std::cout << "oldsize: " << oldsize << std::endl;
+			std::cout << "newsize: " << newsize << std::endl;
+			if (newsize < oldsize) {
+				std::cout << "continuing" << std::endl;
+				cntk = std::move(decomp_ntk);
+				ctu = true;
+			} else {
+				std::cout << "not continuing" << std::endl;
+			}
+		} while (ctu);
+
+		return std::move(cntk);
+	}
+
 }
